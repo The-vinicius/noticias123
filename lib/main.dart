0e25140a511525ada227689c3dfa0de2aeb723d7
The-vinicuis <viniciusnao41@gmail.com>
2025-03-13 19:34:37 -0300

    supabase config

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:noticias123/src/app_module.dart';
 import 'package:noticias123/src/app_widget.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: const String.fromEnvironment('SUPABASE_URL'),
+    anonKey: const String.fromEnvironment('SUPABASE_ANON_KEY'),
+  );
   runApp(ModularApp(
     module: AppModule(),
     child: const AppWidget(),
---
+++ lib/src/app_config.dart
@@ -0,0 +1,3 @@
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+
+final supabese = Supabase.instance.client;
