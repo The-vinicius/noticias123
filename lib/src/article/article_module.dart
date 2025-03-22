9bfa7a64463ba54d01d32a826bfd1c96595a6993
The-vinicuis <viniciusnao41@gmail.com>
2025-03-22 12:03:27 -0300

    article module

---
+++ lib/src/article/article_module.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:noticias123/src/article/data/repositories/article_repository.dart';
+import 'package:noticias123/src/article/data/repositories/article_repository_remote.dart';
+import 'package:noticias123/src/article/data/services/local_data_source.dart';
+import 'package:noticias123/src/article/data/services/remote_data_source.dart';
+import 'package:noticias123/src/article/ui/view_models/article_viewmodel.dart';
+import 'package:noticias123/src/article/ui/widgets/article_detail_screen.dart';
+import 'package:noticias123/src/article/ui/widgets/article_screen.dart';
+import 'package:noticias123/src/article/ui/widgets/form_screen.dart';
+
+class ArticleModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addSingleton(LocalDataSource.new);
+    i.addSingleton(RemoteDataSource.new);
+    i.addSingleton<ArticleRepository>(ArticleRepositoryRemote.new);
+    i.addSingleton(ArticleViewModel.new);
+  }
+
+  @override
+  void routes(RouteManager r) {
+    r.child(
+      '/',
+      child: (_) => ArticlesScreen(
+        articleViewModel: Modular.get<ArticleViewModel>(),
+      ),
+    );
+    r.child('/detail', child: (_) => const ArticleDetailPage());
+    r.child('/form', child: (_) => const FormScreen());
+  }
+}
