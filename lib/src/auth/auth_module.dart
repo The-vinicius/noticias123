66a998939e3012ef111a52183403cccafdd74a6a
The-vinicuis <viniciusnao41@gmail.com>
2025-03-24 21:51:26 -0300

    injection google

--- lib/src/auth/auth_module.dart
+++ lib/src/auth/auth_module.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:noticias123/src/auth/data/repositories/auth_repository.dart';
 import 'package:noticias123/src/auth/data/repositories/auth_repository_remote.dart';
+import 'package:noticias123/src/auth/data/services/google_auth.dart';
 import 'package:noticias123/src/auth/ui/auth_page.dart';
 import 'package:noticias123/src/auth/data/services/supabe_user.dart';
 import 'package:noticias123/src/auth/ui/view_models/login_viewmodel.dart';
+
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthModule extends Module {
   @override
@@ -15,6 +18,10 @@ class AuthModule extends Module {
 
   @override
   void binds(Injector i) {
+    i.addInstance(GoogleSignIn(
+        scopes: ['email', 'profile'],
+        clientId: const String.fromEnvironment('clientId')));
+    i.addSingleton(GoogleAuth.new);
     i.addSingleton(SupabaseUser.new);
   }
 
