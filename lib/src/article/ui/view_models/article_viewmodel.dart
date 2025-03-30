4399f395340622f5330e30665e274598011e56f3
The-vinicuis <viniciusnao41@gmail.com>
2025-03-29 22:41:19 -0300

    rm create new

--- lib/src/article/ui/view_models/article_viewmodel.dart
+++ lib/src/article/ui/view_models/article_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:noticias123/src/article/data/repositories/article_repository.dart';
 import 'package:noticias123/src/article/domian/models/article.dart';
@@ -6,9 +8,9 @@ import 'package:result_dart/result_dart.dart';
 class ArticleViewModel extends ChangeNotifier {
   final ArticleRepository _articleRepository;
   ArticleViewModel(this._articleRepository);
+
   List<Article> _articles = [];
   List<Article> get articles => _articles;
-  bool _success = false;
   String _message = '';
   bool _noData = false;
   bool get noData => _noData;
@@ -16,31 +18,15 @@ class ArticleViewModel extends ChangeNotifier {
   bool _error = false;
   bool _loading = false;
   bool get loading => _loading;
-  bool get success => _success;
   bool get error => _error;
 
   Future<void> getArticles() async {
+    _loading = true;
     final result = _articleRepository.getArticles(1, 10);
     result.fold(
       (ar) {
         _articles = ar;
         _noData = _articles.isEmpty;
-        notifyListeners();
-      },
-      (f) {
-        _error = true;
-        _message = f.toString();
-        notifyListeners();
-      },
-    );
-  }
-
-  Future<void> createArticle(Article article) async {
-    _loading = true;
-    final result = _articleRepository.saveArticle(article);
-    result.fold(
-      (s) {
-        _success = true;
         notifyListeners();
       },
       (f) {
