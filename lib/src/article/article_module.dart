915a958e0402014b7b33c2a0a94834e5289172cf
The-vinicuis <viniciusnao41@gmail.com>
2025-04-04 21:35:48 -0300

    storageservice and postarticleviewmodel addsingleton

--- lib/src/article/article_module.dart
+++ lib/src/article/article_module.dart
@@ -3,7 +3,9 @@ import 'package:noticias123/src/article/data/repositories/article_repository.dar
 import 'package:noticias123/src/article/data/repositories/article_repository_remote.dart';
 import 'package:noticias123/src/article/data/services/local_data_source.dart';
 import 'package:noticias123/src/article/data/services/remote_data_source.dart';
+import 'package:noticias123/src/article/data/services/storage_service.dart';
 import 'package:noticias123/src/article/ui/view_models/article_viewmodel.dart';
+import 'package:noticias123/src/article/ui/view_models/post_article_viewmodel.dart';
 import 'package:noticias123/src/article/ui/widgets/article_detail_screen.dart';
 import 'package:noticias123/src/article/ui/widgets/article_screen.dart';
 import 'package:noticias123/src/article/ui/widgets/form_screen.dart';
@@ -13,8 +15,10 @@ class ArticleModule extends Module {
   void binds(Injector i) {
     i.addSingleton(LocalDataSource.new);
     i.addSingleton(RemoteDataSource.new);
+    i.addSingleton(StorageService.new);
     i.addSingleton<ArticleRepository>(ArticleRepositoryRemote.new);
     i.addSingleton(ArticleViewModel.new);
+    i.addSingleton(PostArticleViewmodel.new);
   }
 
   @override
