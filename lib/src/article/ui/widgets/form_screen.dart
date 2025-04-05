5df8925266e20ec3912be18383d2698916a086f2
The-vinicuis <viniciusnao41@gmail.com>
2025-04-04 21:37:03 -0300

    publish article screen

--- lib/src/article/ui/widgets/form_screen.dart
+++ lib/src/article/ui/widgets/form_screen.dart
@@ -1,8 +1,12 @@
 // modules/form/form_screen.dart
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:noticias123/src/app_config.dart';
+import 'package:noticias123/src/article/data/services/filepicker_image_service.dart';
 import 'package:noticias123/src/article/domian/models/article.dart';
-import 'package:noticias123/src/article/ui/view_models/article_viewmodel.dart';
+import 'package:noticias123/src/article/ui/view_models/post_article_viewmodel.dart';
 
 class FormScreen extends StatefulWidget {
   const FormScreen({super.key});
@@ -15,23 +19,27 @@ class _FormScreenState extends State<FormScreen> {
   final _formKey = GlobalKey<FormState>();
   final _titleController = TextEditingController();
   final _contentController = TextEditingController();
-  final _thumbnailController = TextEditingController();
-  String _selectedCategory = 'Geral';
-  final viewModel = Modular.get<ArticleViewModel>();
+  File? image;
+  final _selectedCategory = 'Geral';
+  final viewModel = Modular.get<PostArticleViewmodel>();
 
   @override
   void initState() {
     super.initState();
+
     viewModel.addListener(() {
       if (viewModel.success) {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Artigo publicado com sucesso')),
         );
         _formKey.currentState!.reset();
+        setState(() {
+          image = null;
+        });
       }
       if (viewModel.error) {
         ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Erro ao publicar o artigo')),
+          SnackBar(content: Text(viewModel.message)),
         );
       }
     });
@@ -39,69 +47,116 @@ class _FormScreenState extends State<FormScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _titleController,
-                decoration: const InputDecoration(labelText: 'Título'),
-                validator: (value) =>
-                    value!.isEmpty ? 'Campo obrigatório' : null,
-              ),
-              const SizedBox(height: 20),
-              DropdownButtonFormField<String>(
-                value: _selectedCategory,
-                items: const [
-                  DropdownMenuItem(value: 'Geral', child: Text('Geral')),
-                  DropdownMenuItem(
-                      value: 'Tecnologia', child: Text('Tecnologia')),
-                  DropdownMenuItem(value: 'Esportes', child: Text('Esportes')),
-                ],
-                onChanged: (value) => _selectedCategory = value!,
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: _contentController,
-                decoration: const InputDecoration(labelText: 'Conteúdo'),
-                maxLines: 5,
-                validator: (value) =>
-                    value!.isEmpty ? 'Campo obrigatório' : null,
-              ),
-              TextFormField(
-                controller: _thumbnailController,
-                decoration: const InputDecoration(labelText: 'url da imagem'),
-                validator: (value) =>
-                    value!.isEmpty ? 'Campo obrigatório' : null,
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    final article = Article(
-                      title: _titleController.text,
-                      content: _contentController.text,
-                      authorId: 'user123', // ID do usuário logado
-                      thumbnailUrl: _thumbnailController.text,
-                      publishedAt: DateTime.now(),
-                      category: _selectedCategory,
-                      status: 'rascunho',
-                      views: 0,
-                      likes: 0,
-                    );
+    return ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, chilf) {
+          return SafeArea(
+            child: viewModel.loading
+                ? const Center(child: CircularProgressIndicator())
+                : Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: SingleChildScrollView(
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          spacing: 10,
+                          children: [
+                            Row(
+                              children: [
+                                const Spacer(),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.green.shade100,
+                                    foregroundColor: Colors.green.shade800,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    elevation: 0,
+                                  ),
+                                  onPressed: () async {
+                                    if (image == null) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(const SnackBar(
+                                              content: Text(
+                                                  'selecione uma imagem')));
+                                      return;
+                                    }
+                                    if (_formKey.currentState!.validate()) {
+                                      final article = Article(
+                                        title: _titleController.text,
+                                        content: _contentController.text,
+                                        authorId: supabase.auth.currentUser!
+                                            .id, // ID do usuário logado
+                                        thumbnailUrl: '',
+                                        publishedAt: DateTime.now(),
+                                        category: _selectedCategory,
+                                        status: 'rascunho',
+                                        views: 0,
+                                        likes: 0,
+                                      );
 
-                    await viewModel.createArticle(article);
-                  }
-                },
-                child: const Text('Publicar Artigo'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                                      await viewModel.createArticle(
+                                          article, image!);
+                                    }
+                                  },
+                                  child: const Text('Publicar'),
+                                ),
+                              ],
+                            ),
+                            TextFormField(
+                              controller: _titleController,
+                              decoration: const InputDecoration(
+                                hintText: 'Title',
+                                border: InputBorder.none,
+                              ),
+                              style: const TextStyle(
+                                fontSize: 32,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Georgia',
+                              ),
+                              validator: (value) =>
+                                  value!.isEmpty ? 'Campo obrigatório' : null,
+                            ),
+                            InkWell(
+                              onTap: () async {
+                                image = await FilePickerSelectImageService()
+                                    .select();
+                                setState(() {});
+                              },
+                              child: image != null
+                                  ? ClipRRect(
+                                      borderRadius: BorderRadius.circular(5),
+                                      child: Image.file(
+                                        image!,
+                                        fit: BoxFit.fitWidth,
+                                        height: 300,
+                                        width: double.infinity,
+                                      ),
+                                    )
+                                  : const Icon(Icons.add_sharp),
+                            ),
+                            const SizedBox(height: 20),
+                            TextFormField(
+                              controller: _contentController,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontFamily: 'Georgia',
+                              ),
+                              decoration: const InputDecoration(
+                                hintText: 'Tell your story...',
+                                border: InputBorder.none,
+                              ),
+                              minLines: 1,
+                              maxLines: null,
+                              validator: (value) =>
+                                  value!.isEmpty ? 'Campo obrigatório' : null,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+          );
+        });
   }
 }
