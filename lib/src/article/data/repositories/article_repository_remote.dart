26126f9060e03fdaa47c7db8ef9cadecbf691f69
The-vinicuis <viniciusnao41@gmail.com>
2025-04-04 21:29:08 -0300

    article repository

--- lib/src/article/data/repositories/article_repository_remote.dart
+++ lib/src/article/data/repositories/article_repository_remote.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:noticias123/src/article/data/exceptions/exceptions.dart';
 import 'package:noticias123/src/article/data/repositories/article_repository.dart';
 import 'package:noticias123/src/article/data/services/local_data_source.dart';
 import 'package:noticias123/src/article/data/services/remote_data_source.dart';
+import 'package:noticias123/src/article/data/services/storage_service.dart';
 import 'package:noticias123/src/article/domian/models/article.dart';
 import 'package:result_dart/result_dart.dart';
 import 'package:result_dart/src/async_result.dart';
@@ -10,26 +13,26 @@ import 'package:result_dart/src/unit.dart';
 class ArticleRepositoryRemote implements ArticleRepository {
   final RemoteDataSource _remoteDataSource;
   final LocalDataSource _localDataSource;
+  final StorageService _storageService;
 
-  ArticleRepositoryRemote(this._remoteDataSource, this._localDataSource);
-
-  @override
-  AsyncResult<Unit, ArticleException> deleteArticle(String id) {
-    // TODO: implement deleteArticle
-    throw UnimplementedError();
-  }
+  ArticleRepositoryRemote(
+      this._remoteDataSource, this._localDataSource, this._storageService);
 
   @override
-  AsyncResult<Article, ArticleException> getArticleById(String id) {
-    // TODO: implement getArticleById
-    throw UnimplementedError();
-  }
-
-  @override
-  AsyncResult<List<Article>, ArticleException> getArticles(
-      int page, int limit) async {
+  AsyncResult<Unit, ArticleException> deleteArticle(int id) async {
     try {
-      final articles = _localDataSource.getArticles();
+      await _remoteDataSource.deleteArticle(id);
+      return const Success(unit);
+    } catch (e) {
+      return Failure(ArticleException(e.toString()));
+    }
+  }
+
+  @override
+  AsyncResult<List<ArticleDone>, ArticleException> getArticleByAuthorId(
+      String id) async {
+    try {
+      final articles = await _remoteDataSource.getArticleByAuthorId(id);
       return Success(articles);
     } catch (e) {
       return Failure(ArticleException(e.toString()));
@@ -37,10 +40,25 @@ class ArticleRepositoryRemote implements ArticleRepository {
   }
 
   @override
-  AsyncResult<List<Article>, ArticleException> getArticlesByCategory(
-      String category) {
-    // TODO: implement getArticlesByCategory
-    throw UnimplementedError();
+  AsyncResult<List<ArticleDone>, ArticleException> getArticles(
+      int page, int limit) async {
+    try {
+      final articles = await _remoteDataSource.getArticles(1, 10);
+      return Success(articles);
+    } catch (e) {
+      return Failure(ArticleException(e.toString()));
+    }
+  }
+
+  @override
+  AsyncResult<List<ArticleDone>, ArticleException> getArticlesByCategory(
+      String category) async {
+    try {
+      final articles = await _remoteDataSource.getArticlesByCategory(category);
+      return Success(articles);
+    } catch (e) {
+      return Failure(ArticleException(e.toString()));
+    }
   }
 
   @override
@@ -56,9 +74,15 @@ class ArticleRepositoryRemote implements ArticleRepository {
   }
 
   @override
-  AsyncResult<Unit, ArticleException> saveArticle(Article article) async {
+  AsyncResult<Unit, ArticleException> saveArticle(
+      Article article, File image) async {
+    final urlResult = await _storageService.uploadFile(image, 'thumbnail');
+    if (urlResult.isError()) {
+      return Failure(ArticleException('Error uploading image'));
+    }
     try {
-      await _localDataSource.saveArticle(article);
+      final newArticle = article.copyWith(thumbnailUrl: urlResult.getOrThrow());
+      await _localDataSource.saveArticle(newArticle);
       return const Success(unit);
     } catch (e) {
       return Failure(ArticleException(e.toString()));
@@ -66,8 +90,23 @@ class ArticleRepositoryRemote implements ArticleRepository {
   }
 
   @override
-  AsyncResult<List<Article>, ArticleException> searchArticles(String query) {
-    // TODO: implement searchArticles
-    throw UnimplementedError();
+  AsyncResult<List<ArticleDone>, ArticleException> searchArticles(
+      String query) async {
+    try {
+      final articles = await _remoteDataSource.searchArticles(query);
+      return Success(articles);
+    } catch (e) {
+      return Failure(ArticleException(e.toString()));
+    }
+  }
+
+  @override
+  AsyncResult<ArticleDone, ArticleException> getArticleById(String id) async {
+    try {
+      final article = await _remoteDataSource.getArticleById(id);
+      return Success(article);
+    } catch (e) {
+      return Failure(ArticleException(e.toString()));
+    }
   }
 }
--- lib/src/article/data/services/remote_data_source.dart
+++ lib/src/article/data/services/remote_data_source.dart
@@ -6,9 +6,38 @@ class RemoteDataSource {
     await supabase.from('articles').insert(article);
   }
 
-  Future<List<Article>> getArticles(int page, int limit) async {
+  Future<List<ArticleDone>> getArticles(int page, int limit) async {
+    final response = await supabase.from('articles').select();
+    return response.map((e) => ArticleDone.fromJson(e)).toList();
+  }
+
+  Future<void> deleteArticle(int id) async {
+    await supabase.from('articles').delete().eq('id', id);
+  }
+
+  Future<void> updateArticle(Article article) async {
+    await supabase.from('articles').update(article.toJson());
+  }
+
+  Future<List<ArticleDone>> getArticlesByCategory(String category) async {
     final response =
-        await supabase.from('articles').select().range(page, limit);
-    return response.map((e) => Article.fromJson(e)).toList();
+        await supabase.from('articles').select().eq('category', category);
+    return response.map((e) => ArticleDone.fromJson(e)).toList();
+  }
+
+  Future<List<ArticleDone>> searchArticles(String query) async {
+    final response =
+        await supabase.from('articles').select().ilike('title', '%$query%');
+    return response.map((e) => ArticleDone.fromJson(e)).toList();
+  }
+
+  Future<List<ArticleDone>> getArticleByAuthorId(String id) async {
+    final response =
+        await supabase.from('articles').select().eq('authorId', id);
+    return response.map((e) => ArticleDone.fromJson(e)).toList();
+  }
+
+  Future<ArticleDone> getArticleById(String id) async {
+    throw '';
   }
 }
